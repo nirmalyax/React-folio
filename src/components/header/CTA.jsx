857eb1a557c6393567abce7977cc300f46b4d202
Nirmalya Mondal <mail@nirmalyax.dev>
2024-02-09 18:44:55 +0530

    Update CTA.jsx
    Signed-off-by: Nirmalya Mondal <mail@nirmalyax.dev>

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import CV from '../../assets/NirmalyaMondal_Resume.pdf';
+//import CV from '../../assets/NirmalyaMondal_Resume.pdf';
 
 const CTA = () => {
   return (
     <div className="cta">
-      <a href={CV} download className="btn">
+      <a href= "https://resumee.nirmalyax.dev/" download className="btn">
         Download CV
       </a>
       <a href="#contact" className="btn btn-primary">
